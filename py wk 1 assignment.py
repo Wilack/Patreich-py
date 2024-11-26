eb0cb8bebd83787807eff20fd9c346e913b431a9
Wilack <ochiengpatriech@gmail.com>
2024-11-26 11:48:43 +0300

    Add files via upload

---
+++ py wk 1 assignment.py
@@ -0,0 +1,21 @@
+print("addition")
+x=int(input("enter a number x:")
+)
+y=int(input("enter a number y:"))
+print( x+y)
+print("subtraction")
+p = int(input("enter a number p"))
+r =int(input("enter a number q"))
+if p>r :
+    print("subration:",p-r)
+else :print("subtraction:",r-p)
+print("multiplication")
+
+q=int(input("input intrger q:"))
+w=int(input ("input interger w:"))
+print (q*w)
+print("division")
+f=int(input("enter value f:"))
+h=int(input("enter value h:"))
+   pr
+
