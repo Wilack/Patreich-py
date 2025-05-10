482fbd95c7c9bf61bea66ef89d9fb92448867b5c
Wilack <ochiengpatriech@gmail.com>
2025-05-10 14:49:56 +0300

    Update py wk 1 assignment.py

--- py wk 1 assignment.py
+++ py wk 1 assignment.py
@@ -1,21 +1,29 @@
 print("addition")
-x=int(input("enter a number x:")
+x=int
+(input("enter a number x:")
 )
-y=int(input("enter a number y:"))
+y=int
+(input("enter a number y:"))
 print( x+y)
 print("subtraction")
-p = int(input("enter a number p"))
-r =int(input("enter a number q"))
+p = int
+ (input("enter a number p"))
+r =int
+ (input("enter a number q"))
 if p>r :
     print("subration:",p-r)
 else :print("subtraction:",r-p)
 print("multiplication")
 
-q=int(input("input intrger q:"))
-w=int(input ("input interger w:"))
+q=int 
+ (input("input intrger q:"))
+w=int
+ (input ("input interger w:"))
 print (q*w)
 print("division")
-f=int(input("enter value f:"))
-h=int(input("enter value h:"))
+f=int
+ (input("enter value f:"))
+h=int
+ (input("enter value h:"))
    pr
 
